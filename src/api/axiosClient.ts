4a8cc874d45e97e14ed2cb617eac17f4926e5c2f
Rehan Khan <rehan.khan.teo@gotoko.co.id>
2024-05-03 11:09:54 +0500

    Move url to env

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,9 +1,7 @@
 import axios from "axios"
 
 const jsonServerAxios = axios.create({
-	// baseURL: "https://official-joke-api.appspot.com/jokes",
-
-	baseURL: "https://663136c1c92f351c03dc8255.mockapi.io/joke",
+	baseURL: import.meta.env.VITE_API_URL,
 
 	headers: {
 		"Content-Type": "application/json",
